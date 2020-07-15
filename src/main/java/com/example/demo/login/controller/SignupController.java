9a5881c5de2beef5680e9d7a03a4745b10078ed5
norio79 <norionorio9279@gmail.com>
2020-07-15 17:51:30 +0900

    データバインド２

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,7 +41,7 @@ public class SignupController {
 	}
 	//ユーザー登録画面のPOST用のコントローラー.
 	@PostMapping("/signup")
-	public String postSignUp(@ModelAttribute SignupForm form, BindingResult bindingResult, Model model) {
+	public String postSignUp(@ModelAttribute @Validated SignupForm form, BindingResult bindingResult, Model model) {
 		
 		//入力チェックに引っ掛かった場合、ユーザー登録画面に戻る
 		if(bindingResult.hasErrors()) {
