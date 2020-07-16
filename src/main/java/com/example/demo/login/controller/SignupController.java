f79d40d5671b84d9abd79c53378c95870e7c7e91
norio79 <norionorio9279@gmail.com>
2020-07-16 21:18:01 +0900

    データバインド　やり直し

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -5,11 +5,11 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import com.example.demo.domain.model.SignupForm;
+
+import com.example.demo.login.domain.model.SignupForm;
 
 @Controller
 public class SignupController {
@@ -41,7 +41,7 @@ public class SignupController {
 	}
 	//ユーザー登録画面のPOST用のコントローラー.
 	@PostMapping("/signup")
-	public String postSignUp(@ModelAttribute @Validated SignupForm form, BindingResult bindingResult, Model model) {
+	public String postSignUp(@ModelAttribute SignupForm form, BindingResult bindingResult, Model model) {
 		
 		//入力チェックに引っ掛かった場合、ユーザー登録画面に戻る
 		if(bindingResult.hasErrors()) {
